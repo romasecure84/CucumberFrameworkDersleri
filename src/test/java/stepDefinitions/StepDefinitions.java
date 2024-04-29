a549cba58491f7ce14741b83f9e90695421bf4f0
ROMA2 <romasecure84@yandex.ru>
2024-04-29 23:10:46 +0400

    cucumber ssenarisiyle test dersi elave olundu

--- src/test/java/stepDefinitions/StepDefinitions.java
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -2,7 +2,9 @@ package stepDefinitions;
 
 import io.cucumber.java.en.*;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
@@ -11,31 +13,29 @@ import utilities.MyDriver;
 import java.time.Duration;
 
 public class StepDefinitions {
-    private  WebDriver driver;
+    private static WebDriver driver;
 
     @Given("go to login page")
     public void go_to_login_page() {
         driver = MyDriver.getDriver();
-        driver.get("https://www.google.az");
+        driver.get("https://practicetestautomation.com/practice-test-login/");
     }
     @Given("type username")
     public void type_username() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver.findElement(By.id("username")).sendKeys("student");
     }
     @Given("type password")
     public void type_password() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver.findElement(By.id("password")).sendKeys("Password123");
     }
     @When("click button")
     public void click_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver.findElement(By.id("submit")).click();
     }
     @Then("assert to success message")
-    public void assert_to_success_message() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void assert_to_success_message() throws InterruptedException {
+        Assert.assertTrue(driver.findElement(By.xpath("//*[text()='Logged In Successfully']")).isDisplayed());
+        Thread.sleep(2000);
+        driver.quit();
     }
 }

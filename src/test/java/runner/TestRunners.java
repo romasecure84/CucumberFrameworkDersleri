a07317d98cdfb3fc36fe43952805fc2cd52198d4
ROMA2 <romasecure84@yandex.ru>
2024-05-02 23:09:07 +0400

    test runner'e html reportlarin qeyd edilmesi elave olundu

--- src/test/java/runner/TestRunners.java
+++ src/test/java/runner/TestRunners.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features="src/test/java/features",
         glue = "stepDefinitions",
-        plugin = {"pretty", "html:target/cucumber-htm-report"}
+        plugin = {"pretty", "html:target/cucumber-reports.html"}
 )
 public class TestRunners {
 
